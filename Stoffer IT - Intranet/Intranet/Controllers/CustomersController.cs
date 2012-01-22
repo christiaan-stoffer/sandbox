1d4e32134430f3291ec94a494206513c5b811f1d
Christiaan Stoffer <christiaan@stofferit.nl>
2012-01-22 23:54:04 +0100

    Created a first customers landing page idea

--- Stoffer IT - Intranet/Intranet/Controllers/CustomersController.cs
+++ Stoffer IT - Intranet/Intranet/Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Intranet.Model;
 
 namespace Intranet.Controllers
 {
@@ -13,7 +14,13 @@ namespace Intranet.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var randomLinks = Enumerable.Range(0, 34).Select(itm => new ImageCustomerLink { ImageUrl = "http://www.google.com/images/srpr/logo3w.png", Name = "Klant " + itm, Url = "#"});
+
+            
+            return View(new CustomerOverview<ImageCustomerLink>
+                          {
+                              Customers = randomLinks
+                          });
         }
 
     }
