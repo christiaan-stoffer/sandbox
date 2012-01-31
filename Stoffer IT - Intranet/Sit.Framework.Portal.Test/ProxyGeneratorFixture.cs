a0530b591219d06a93217cc6ca59013e79aaebbe
Christiaan Stoffer <christiaan@stofferit.nl>
2012-01-31 23:49:04 +0100

    A bit more advanced, still nothing special :)

--- Stoffer IT - Intranet/Sit.Framework.Portal.Test/ProxyGeneratorFixture.cs
+++ Stoffer IT - Intranet/Sit.Framework.Portal.Test/ProxyGeneratorFixture.cs
@@ -12,6 +12,17 @@ namespace Sit.Framework.Portal.Test
             string Name { get; set; }
         }
 
+        public interface ITestObjectAdvanced
+        {
+            Guid Id { get; set; }
+
+            string Name { get; set; }
+
+            string Description { get; set; }
+
+            int Order { get; set; }
+        }
+
         [Test]
         [ExpectedException(typeof(NotSupportedException))]
         public void InvalidProxyTest()
@@ -29,6 +40,24 @@ namespace Sit.Framework.Portal.Test
             p.Name = thisIsAName;
 
             Assert.AreEqual(thisIsAName, p.Name);
+        }
+
+        [Test]
+        public void AdvancedProxyTest()
+        {
+            var p = Proxy.New<ITestObjectAdvanced>();
+
+            p.Id = new Guid("D28AED59-C1BE-4A09-96BC-0B267AE522DA");
+            p.Name = "Test";
+            p.Order = 19382;
+            p.Description = "This is a test object";
+
+            Process(p);
+        }
+
+        private void Process(ITestObjectAdvanced testObjectAdvanced)
+        {
+            // TODO; :)
         }
     }
 }
