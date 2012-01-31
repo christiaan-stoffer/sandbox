69a408f7c412ffcc8f6e87657fac4040c288d7c0
Christiaan Stoffer <christiaan@stofferit.nl>
2012-01-31 20:06:24 +0100

    Remove everything for a new version (poc)

--- Stoffer IT - Intranet/Intranet/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Intranet.MvcApplication" Language="C#" %>
--- Stoffer IT - Intranet/Sit.Intranet.Model.Test/IProduct.cs
+++
@@ -1,9 +0,0 @@
-namespace Sit.Intranet.Model.Test
-{
-    public interface IProduct
-    {
-        string Name { get; set; }
-
-        string Description { get; set; }
-    }
-}
